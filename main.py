722b3b97f48533e83f3fed07fd3125dbf5586510
Husniyor <husniyor09@gmail.com>
2024-12-02 18:47:17 +0500

    initial project

---
+++ main.py
@@ -0,0 +1,77 @@
+TOKEN = "7253041398:AAEb0yGrTvyrRc7E9_O5Uu0OvYFSkD8S3o8"
+ADMIN = 1779062204
+
+#!/usr/bin/env python
+# pylint: disable=unused-argument
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple example of a Telegram WebApp which displays a color picker.
+The static website for this website is hosted by the PTB team for your convenience.
+Currently only showcases starting the WebApp via a KeyboardButton, as all other methods would
+require a bot token.
+"""
+import json
+import logging
+
+from telegram import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, WebAppInfo, BotCommand
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a `/start` command handler.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message with a button that opens the web app."""
+    commands = [BotCommand(command="info",description="Get info about this bot."),
+                BotCommand('start',"start the bot")]
+    context.bot.set_my_commands(commands)
+    await update.message.reply_text(
+        "Hello and welcome to bot created by <b>A.Husniyor</b>. To get rgb colors press button below👇",
+        parse_mode="HTML",
+        reply_markup=ReplyKeyboardMarkup.from_button(
+            KeyboardButton(
+                text="Open the color picker!",
+                web_app=WebAppInfo(url="https://python-telegram-bot.org/static/webappbot"),
+            ),
+        ),
+    )
+    await context.bot.send_message(chat_id=ADMIN,text=f"new user - {update.effective_user.id} - {update.effective_user.username}")
+
+
+# Handle incoming WebAppData
+async def web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Print the received data and remove the button."""
+    # Here we use `json.loads`, since the WebApp sends the data JSON serialized string
+    # (see webappbot.html)
+    data = json.loads(update.effective_message.web_app_data.data)
+    await update.message.reply_html(
+        text=(
+            f"You selected the color with the HEX value <code>{data['hex']}</code>. The "
+            f"corresponding RGB value is <code>{tuple(data['rgb'].values())}</code>."
+        ),
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
